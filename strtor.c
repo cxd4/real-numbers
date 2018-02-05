650aa7a95f81b9c63680ccbdead1c4dd29ae6b27
Iconoclast <cxd4@uniplex.cat-vs-internet.gov>
2018-02-05 11:49:52 -0500

    Recognize Euler's number from user input.

--- strtor.c
+++ strtor.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 
 #include <stdlib.h>
+#include <string.h>
+
+#include <math.h>
 #include "strtor.h"
 
 real
@@ -13,7 +16,14 @@ strtor(const char* str)
         fprintf(stderr, "Unchecked errno already set to %i.\n", errno);
         errno = 0;
     }
-    result = strtod(str, NULL);
+    if (str[0] == 'e') {
+        if (str[1] == '\0')
+            result = exp(1);
+        else
+            result = exp(strtor(str + 1));
+    } else {
+        result = strtod(str, NULL);
+    }
     if (errno) {
         fprintf(
             stderr, "Problem %i converting %s to %f.\n",
