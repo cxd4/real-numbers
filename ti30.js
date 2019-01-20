c13fae3f4dacdf08e120862a9814c77156d7b039
Iconoclast <cxd4@member.fsf.org>
2019-01-20 16:23:22 -0500

    added emulations of TI-30 memory store and recall

--- ti30.js
+++ ti30.js
@@ -20,6 +20,8 @@
  */
 
 var exp = "0";
+var stored_reg = null; /* user-defined temporary register via STO/RCL/SUM/EXC */
+
 var display = ""; /* line displayed while typing in digits or decimal point */
 
 /*
@@ -209,6 +211,9 @@ function ti_clear() {
     display = "";
     exp = "";
     document.getElementById("memory_reg").innerHTML = "0";
+
+    stored_reg = null;
+    document.getElementById("M_register").innerHTML = "";
     return;
 }
 function backspace() {
@@ -221,5 +226,34 @@ function backspace() {
     display = display.substring(1);
     exp = exp.substring(1);
     document.getElementById("memory_reg").innerHTML = display;
+    return;
+}
+
+function ti_store() {
+    "use strict";
+
+    stored_reg = Number(display);
+    document.getElementById("M_register").innerHTML = "M";
+
+    display = "";
+    is_evaluated_const = true;
+    return;
+}
+function ti_recall() {
+    "use strict";
+
+    display = String(stored_reg);
+    if (stored_reg === null) {
+        display = "0";
+    }
+    document.getElementById("memory_reg").innerHTML = display;
+
+    if (is_evaluated_const === true) {
+        exp = display;
+    } else {
+        exp += display;
+    }
+    display = "";
+    is_evaluated_const = true;
     return;
 }
